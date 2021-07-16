f87271889323dc2eaa7ff710a0ecbb97df206d51
TiberghienValerie <tiberghien.valerie@wanadoo.fr>
2021-07-16 11:00:44 +0200

    routage

--- public/index.php
+++ public/index.php
@@ -14,4 +14,14 @@ $pokedex = new \Hb\BasicPokeapi\Pokedex();
 header('Content-Type: application/json');
 
 
-echo json_encode($pokedex->getAllPokemon());
+if (!isset($_SERVER['PATH_INFO']) || '/pokemon' === $_SERVER['PATH_INFO']) {
+    // Query data with Pokedex and convert to JSON to be send to the client.
+    echo json_encode($pokedex->getAllPokemon());
+} elseif ('/pikachu' === $_SERVER['PATH_INFO']) {
+    echo json_encode($pokedex->getCleanPikachu());
+} else {
+    http_response_code(404);
+    echo json_encode([
+        'error' => 'Wrong path.',
+    ]);
+}
